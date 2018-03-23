b56b7be92b59fa3760991401fea0e01d0858b9d7
veken1199 <super_veko@yahoo.com>
2018-03-22 21:54:42 -0400

    related to #108 and #111: modified old tables like sms and mms and general code improvements

--- src/org/thoughtcrime/securesms/database/MessageReactionDatabase.java
+++ src/org/thoughtcrime/securesms/database/MessageReactionDatabase.java
@@ -8,35 +8,35 @@ import android.util.Log;
 
 import org.thoughtcrime.securesms.database.model.MessageRecord;
 
-import java.util.HashMap;
-
-
 public class MessageReactionDatabase extends Database {
-
     private static final String TAG = MessageReactionDatabase.class.getSimpleName();
 
-    public static final String TABLE_NAME = "reactions";
-    public static final String ID         = "id";
-    public static final String SMS_ID     = "sms_id";
-    public static final String MMS_ID     = "mms_id";
-    public static final String REACTION   = "reaction";
-    public static final String REACTOR_ID = "reactor_id";
-    public static final String HASH_ID    = "hash";
+    public static final String TABLE_NAME    = "reactions";
+    public static final String ID            = "id";
+    public static final String SMS_HASH      = "sms_id";
+    public static final String MMS_HASH      = "mms_id";
+    public static final String REACTION      = "reaction";
+    public static final String REACTOR_ID    = "reactor_id";
+    public static final String HASH_ID       = "hash";
+    public static final String REACTION_DATE = "date_sent";
 
     public static final String[] PROJECTION = {MessageReactionDatabase.REACTION,
-            MessageReactionDatabase.SMS_ID,
-            MessageReactionDatabase.MMS_ID,
-            MessageReactionDatabase.REACTION,
-            MessageReactionDatabase.REACTOR_ID};
+                                                MessageReactionDatabase.SMS_HASH,
+                                                MessageReactionDatabase.MMS_HASH,
+                                                MessageReactionDatabase.REACTION,
+                                                MessageReactionDatabase.REACTOR_ID,
+                                                MessageReactionDatabase.REACTION_DATE,
+                                                MessageReactionDatabase.HASH_ID};
 
     public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "(" + ID + "  INTEGER PRIMARY KEY, " +
-            SMS_ID + " INTEGER DEFAULT NULL, " +
-            MMS_ID + " INTEGER DEFAULT NULL, " +
+            SMS_HASH + " TEXT DEFAULT NULL, " +
+            MMS_HASH + " TEXT DEFAULT NULL, " +
             REACTION + " TEXT NOT NULL, " +
             REACTOR_ID + " TEXT NOT NULL, " +
-            HASH_ID + " TEXT NOT NULL, " +
-            "FOREIGN KEY(" + SMS_ID + ") REFERENCES " + SmsDatabase.TABLE_NAME + "(" + SmsDatabase.ID + ")," +
-            "FOREIGN KEY(" + MMS_ID + ") REFERENCES " + MmsDatabase.TABLE_NAME + "(" + MmsDatabase.ID + "));";
+            HASH_ID + " TEXT, " +
+            REACTION_DATE + " INTEGER NOT NULL, " +
+            "FOREIGN KEY(" + SMS_HASH + ") REFERENCES " + SmsDatabase.TABLE_NAME + "(" + SmsDatabase.HASH + ")," +
+            "FOREIGN KEY(" + MMS_HASH + ") REFERENCES " + MmsDatabase.TABLE_NAME + "(" + MmsDatabase.HASH + "));";
 
     public MessageReactionDatabase(Context context, SQLiteOpenHelper databaseHelper) {
         super(context, databaseHelper);
@@ -46,14 +46,14 @@ public class MessageReactionDatabase extends Database {
         Log.w("MessageDatabase", "reacting to: " + messageRecord.getId());
 
         ContentValues values = new ContentValues();
-        String messageType;
+        String        messageType;
 
         if (messageRecord.isMms()) {
-            values.put(MMS_ID, messageRecord.getId());
-            messageType = MMS_ID;
+            values.put(MMS_HASH, messageRecord.getId());
+            messageType = MMS_HASH;
         } else {
-            values.put(SMS_ID, messageRecord.getId());
-            messageType = SMS_ID;
+            values.put(SMS_HASH, messageRecord.getId());
+            messageType = SMS_HASH;
         }
         values.put(REACTION, reaction);
         values.put(REACTOR_ID, reactorId);
@@ -61,11 +61,11 @@ public class MessageReactionDatabase extends Database {
         insertOrUpdate(values, messageRecord.getId(), messageType);
     }
 
-    private int processPinSqlRequest(ContentValues values, long messageId, String messageType) {
+    private int processReactionSqlRequest(ContentValues values, long messageId, String messageType) {
         SQLiteDatabase db = databaseHelper.getWritableDatabase();
         return db.update(TABLE_NAME, values, messageType + " = ? AND " +
-                REACTOR_ID + " = ? ",
-                new String[] {values.getAsInteger(messageType) + "", values.getAsString(REACTOR_ID)});
+                        REACTOR_ID + " = ? ",
+                new String[]{values.getAsInteger(messageType) + "", values.getAsString(REACTOR_ID)});
     }
 
     private void insertOrUpdate(ContentValues values, long messageId, String messageType) {
@@ -75,7 +75,7 @@ public class MessageReactionDatabase extends Database {
 
         int updated = database.update(TABLE_NAME, values, messageType + " = ? AND " +
                         REACTOR_ID + " = ? ",
-                new String[] {values.getAsInteger(messageType) + "", values.getAsString(REACTOR_ID)});
+                new String[]{values.getAsInteger(messageType) + "", values.getAsString(REACTOR_ID)});
 
         if (updated < 1) {
             database.insert(TABLE_NAME, null, values);

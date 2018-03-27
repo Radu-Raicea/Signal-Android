e2aba69eb7882c20f3b6a56712c2d0befc0333fd
veken1199 <super_veko@yahoo.com>
2018-03-26 23:18:08 -0400

    related to #108 and #111: Extended MessageReactionDatabase functionality 1- added method to remove reaction of a given messagerecord 2- added method to clear/remove dangling message hashes. this method to be used when the app is starded 3- added method to get the reactions of a given message

--- src/org/thoughtcrime/securesms/database/MessageReactionDatabase.java
+++ src/org/thoughtcrime/securesms/database/MessageReactionDatabase.java
@@ -2,8 +2,12 @@ package org.thoughtcrime.securesms.database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Message;
+import android.os.Parcel;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import org.thoughtcrime.securesms.database.model.MessageRecord;
@@ -21,61 +25,85 @@ public class MessageReactionDatabase extends Database {
     public static final String REACTION_DATE = "date_sent";
 
     public static final String[] PROJECTION = {MessageReactionDatabase.REACTION,
-                                                MessageReactionDatabase.SMS_HASH,
-                                                MessageReactionDatabase.MMS_HASH,
-                                                MessageReactionDatabase.REACTION,
-                                                MessageReactionDatabase.REACTOR_ID,
-                                                MessageReactionDatabase.REACTION_DATE,
-                                                MessageReactionDatabase.HASH_ID};
+            MessageReactionDatabase.SMS_HASH,
+            MessageReactionDatabase.MMS_HASH,
+            MessageReactionDatabase.REACTION,
+            MessageReactionDatabase.REACTOR_ID,
+            MessageReactionDatabase.REACTION_DATE,
+            MessageReactionDatabase.HASH_ID};
 
     public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "(" + ID + "  INTEGER PRIMARY KEY, " +
             SMS_HASH + " TEXT DEFAULT NULL, " +
             MMS_HASH + " TEXT DEFAULT NULL, " +
             REACTION + " TEXT NOT NULL, " +
             REACTOR_ID + " TEXT NOT NULL, " +
-            HASH_ID + " TEXT, " +
             REACTION_DATE + " INTEGER NOT NULL, " +
-            "FOREIGN KEY(" + SMS_HASH + ") REFERENCES " + SmsDatabase.TABLE_NAME + "(" + SmsDatabase.HASH + ")," +
-            "FOREIGN KEY(" + MMS_HASH + ") REFERENCES " + MmsDatabase.TABLE_NAME + "(" + MmsDatabase.HASH + "));";
+            "FOREIGN KEY(" + SMS_HASH + ") REFERENCES " + SmsDatabase.TABLE_NAME + "(" + SmsDatabase.HASH + ") ON DELETE CASCADE," +
+            "FOREIGN KEY(" + MMS_HASH + ") REFERENCES " + MmsDatabase.TABLE_NAME + "(" + MmsDatabase.HASH + ") ON DELETE CASCADE);";
 
     public MessageReactionDatabase(Context context, SQLiteOpenHelper databaseHelper) {
         super(context, databaseHelper);
     }
 
-    public void reactToMessage(MessageRecord messageRecord, String reaction, int reactorId) {
-        Log.w("MessageDatabase", "reacting to: " + messageRecord.getId());
-
-        ContentValues values = new ContentValues();
+    public void reactToMessage(MessageRecord messageRecord, String reaction, Long reactionDate) {
         String        messageType;
+        ContentValues values = new ContentValues();
+        
+        messageType = getMessageType(messageRecord);
+        values.put(messageType, messageRecord.getHash());
 
+        Address address;
+        if (messageRecord.isOutgoing()) {
+            try {
+                address = DatabaseFactory.getIdentityDatabase(context).getMyIdentity().getAddress();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Parcel p = Parcel.obtain();
+                p.writeString("Me");
+                address = new Address(p);
+            }
+        } else {
+            address = messageRecord.getRecipient().getAddress();
+        }
+
+        values.put(REACTION, reaction);
+        values.put(REACTOR_ID, address.serialize());
+        values.put(REACTION_DATE, reactionDate);
+        insertOrUpdate(values, messageType);
+    }
+
+    @NonNull
+    private String getMessageType(MessageRecord messageRecord) {
+        String messageType;
         if (messageRecord.isMms()) {
-            values.put(MMS_HASH, messageRecord.getId());
             messageType = MMS_HASH;
         } else {
-            values.put(SMS_HASH, messageRecord.getId());
             messageType = SMS_HASH;
         }
-        values.put(REACTION, reaction);
-        values.put(REACTOR_ID, reactorId);
-
-        insertOrUpdate(values, messageRecord.getId(), messageType);
+        return messageType;
     }
 
-    private int processReactionSqlRequest(ContentValues values, long messageId, String messageType) {
+    private int processReactionSqlRequest(ContentValues values, String messageHash, String messageType) {
         SQLiteDatabase db = databaseHelper.getWritableDatabase();
         return db.update(TABLE_NAME, values, messageType + " = ? AND " +
-                        REACTOR_ID + " = ? ",
-                new String[]{values.getAsInteger(messageType) + "", values.getAsString(REACTOR_ID)});
+                        REACTOR_ID + " = ? AND " +
+                        REACTION + " = ?",
+                new String[]{values.getAsInteger(messageType) + "",
+                        values.getAsString(REACTOR_ID),
+                        values.getAsString(REACTION)});
     }
 
-    private void insertOrUpdate(ContentValues values, long messageId, String messageType) {
+    private void insertOrUpdate(ContentValues values, String messageType) {
         SQLiteDatabase database = databaseHelper.getWritableDatabase();
 
         database.beginTransaction();
 
         int updated = database.update(TABLE_NAME, values, messageType + " = ? AND " +
-                        REACTOR_ID + " = ? ",
-                new String[]{values.getAsInteger(messageType) + "", values.getAsString(REACTOR_ID)});
+                        REACTOR_ID + " = ? AND " +
+                        REACTION + " = ?",
+                new String[]{values.getAsString(messageType) + "",
+                        values.getAsString(REACTOR_ID),
+                        values.getAsString(REACTION)});
 
         if (updated < 1) {
             database.insert(TABLE_NAME, null, values);
@@ -85,4 +113,32 @@ public class MessageReactionDatabase extends Database {
         database.endTransaction();
     }
 
+    private int  removeReaction(MessageRecord record) {
+        SQLiteDatabase database = databaseHelper.getWritableDatabase();
+
+        return database.delete(TABLE_NAME, getMessageType(record) + " = ?", new String[]{record.getHash()});
+    }
+
+    public int removeDanglingSmsReactions() {
+        SQLiteDatabase database = databaseHelper.getWritableDatabase();
+
+        return database.delete(TABLE_NAME,SMS_HASH + " in (SELECT " + SMS_HASH + " FROM " + TABLE_NAME + " OUTER LEFT JOIN "
+                        + SmsDatabase.TABLE_NAME + " ON " + TABLE_NAME + "." + SMS_HASH + " = sms.hash WHERE sms.hash is null)",
+                new String[]{});
+    }
+
+    public int removeDanglingMmsReactions() {
+        SQLiteDatabase database = databaseHelper.getWritableDatabase();
+
+        return database.delete(TABLE_NAME,MMS_HASH + " in (SELECT " + MMS_HASH+ " FROM " + TABLE_NAME + " OUTER LEFT JOIN "
+                        + MmsDatabase.TABLE_NAME + " ON " + TABLE_NAME + "." + MMS_HASH + " = mms.hash WHERE mms.hash is null)",
+                new String[]{});
+    }
+
+    public Cursor getMessageReaction(MessageRecord messageRecord) {
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
+
+        return database.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " +
+                getMessageType(messageRecord) + " = ?", new String[]{messageRecord.getHash()});
+    }
 }

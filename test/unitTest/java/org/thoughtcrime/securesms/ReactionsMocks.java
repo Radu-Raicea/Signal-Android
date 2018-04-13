05ca05b8df75570e853edefc7bd75408cbcba9b4
Erin <erin.benderoff@gmail.com>
2018-04-13 01:21:36 -0400

    #115 finished reaction handler mocks and unit tests

--- test/unitTest/java/org/thoughtcrime/securesms/ReactionsMocks.java
+++ test/unitTest/java/org/thoughtcrime/securesms/ReactionsMocks.java
@@ -1,9 +1,12 @@
 package org.thoughtcrime.securesms;
 
 import android.database.Cursor;
+import android.os.Parcel;
 
 import org.junit.runner.RunWith;
+import org.mockito.AdditionalAnswers;
 import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -11,51 +14,87 @@ import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.MessageReactionDatabase;
 import org.thoughtcrime.securesms.database.model.MessageRecord;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({DatabaseFactory.class})
+@PrepareForTest({DatabaseFactory.class, Parcel.class})
 public class ReactionsMocks extends BaseUnitTest {
-    protected DatabaseFactory databaseFactory;
-    protected MessageReactionDatabase messageReactionDatabase;
-    protected MessageRecord messageRecord;
-    protected String reaction;
-    protected String messageHash;
-    protected String reactorId;
-    protected Long reactionDate;
-    protected Long threadId;
-    protected Cursor cursor;
+    protected DatabaseFactory                 databaseFactory;
+    protected MessageReactionDatabase         messageReactionDatabase;
+    protected MessageRecord                   messageRecord;
+    protected Cursor                          cursor;
+    protected Parcel                          parcel;
+    protected String[] reactions;
+    protected String                          reactionId;
+    protected String                          messageHash;
+    protected String                          reactorId;
+    protected String                          messageType;
+    protected Long                            reactionDate;
+    protected Long                            threadId;
 
     @Override
     public void setUp() throws Exception {
-        databaseFactory = mock(DatabaseFactory.class);
+        databaseFactory         = mock(DatabaseFactory.class);
         messageReactionDatabase = mock(MessageReactionDatabase.class);
-        messageRecord = mock(MessageRecord.class);
-        cursor = mock(Cursor.class);
+        messageRecord           = mock(MessageRecord.class);
+        cursor                  = mock(Cursor.class);
+        parcel                  = mock(Parcel.class);
+        reactions               = new String[3];
+        reactionId              = "reaction id";
+        messageHash             = "message hash";
+        reactorId               = "reactor id";
+        messageType             = "SMS";
+        reactionDate            = 1234L;
+        threadId                = 1L;
     }
 
-    public void setUpReactToMessageSender() {
-
-    }
-
-    public void setUpReactToMessageReceiver() {
-
-    }
-
-    public void setUpDeleteReaction() {
-        when(messageReactionDatabase.removeReaction(messageRecord)).thenReturn(1);
+    public void setUpTestReactions() {
+        reactions[0] = ":)";
+        reactions[1] = ":(";
+        reactions[2] = ":)";
     }
 
     public void setUpGetMessageReactions() {
         when(messageReactionDatabase.getMessageReaction(messageRecord)).thenReturn(cursor);
     }
 
-    public void setUpStaticMessageReactionDatabase() {
+    public void setUpStaticMessageReactionDatabase() throws Exception {
         PowerMockito.mockStatic(DatabaseFactory.class);
         BDDMockito.given(DatabaseFactory.getInstance(context)).willReturn(databaseFactory);
         BDDMockito.given(DatabaseFactory.getMessageReactionDatabase(context)).willReturn(messageReactionDatabase);
     }
 
+    public void setUpParcel() {
+        PowerMockito.mockStatic(Parcel.class);
+        when(Parcel.obtain()).thenReturn(parcel);
+        when(parcel.readString()).thenReturn(reactorId);
+    }
+
+    public void setUpCursor() {
+        when(cursor.moveToNext())
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(false);
+
+        when(cursor.getLong(cursor.getColumnIndexOrThrow(Mockito.anyString())))
+                .thenReturn(reactionDate);
+
+        List<String> reactionAttributes = new ArrayList<>();
+
+        for (String reaction : reactions) {
+            reactionAttributes.add(reactorId);
+            reactionAttributes.add(reaction);
+            reactionAttributes.add(reactionId);
+            reactionAttributes.add(messageType);
+            reactionAttributes.add(messageHash);
+        }
+
+        when(cursor.getString(cursor.getColumnIndexOrThrow(Mockito.anyString())))
+                .thenAnswer(AdditionalAnswers.returnsElementsOf(reactionAttributes));
+    }
 }
--- test/unitTest/java/org/thoughtcrime/securesms/messagereactions/ReactionsHandlerTests.java
+++ test/unitTest/java/org/thoughtcrime/securesms/messagereactions/ReactionsHandlerTests.java
@@ -6,6 +6,10 @@ import org.thoughtcrime.securesms.ReactionsHandler;
 import org.thoughtcrime.securesms.ReactionsMocks;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 
+import java.util.List;
+import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 
 public class ReactionsHandlerTests extends ReactionsMocks {
@@ -15,30 +19,48 @@ public class ReactionsHandlerTests extends ReactionsMocks {
     @Before
     public void setUp() throws Exception {
         super.setUp();
+        super.setUpTestReactions();
+        super.setUpGetMessageReactions();
+        super.setUpStaticMessageReactionDatabase();
+        super.setUpParcel();
+        super.setUpCursor();
         reactionsHandler = new ReactionsHandler(context);
         reactionsHandler.setMessageReactionDatabase(DatabaseFactory.getMessageReactionDatabase(context));
     }
 
     @Test
     public void testAddReactionToSenderDB() {
-        reactionsHandler.addReactionToSenderDB(messageRecord, reaction, reactionDate);
-        verify(messageReactionDatabase).reactToMessage(messageRecord, reaction, reactionDate);
+        reactionsHandler.addReactionToSenderDB(messageRecord, reactions[0], reactionDate);
+        verify(messageReactionDatabase).reactToMessage(messageRecord, reactions[0], reactionDate);
     }
 
     @Test
     public void testAddReactionToReceiverDB() {
-        reactionsHandler.addReactionToReceiverDB(messageHash, reaction, reactionDate, reactorId, threadId);
-        verify(messageReactionDatabase).reactToMessage(messageHash, reaction, reactionDate, reactorId, threadId);
-    }
-
-    @Test
-    public void testDeleteReaction() {
-        reactionsHandler.deleteReaction(messageRecord);
-
+        reactionsHandler.addReactionToReceiverDB(messageHash, reactions[0], reactionDate, reactorId, threadId);
+        verify(messageReactionDatabase).reactToMessage(messageHash, reactions[0], reactionDate, reactorId, threadId);
     }
 
     @Test
     public void testGetMessageReactions() {
+        List<ReactionsHandler.Reaction> results = reactionsHandler.getMessageReactions(messageRecord);
 
+        assertEquals(results.size(), 3);
+
+        assertEquals(results.get(0).getReaction(), ":)");
+        assertEquals(results.get(1).getReaction(), ":(");
+        assertEquals(results.get(2).getReaction(), ":)");
+
+        assertEquals(results.get(0).getReactor().toString(), "reactor id");
+        assertEquals(results.get(0).getReactionDate(), (Long) 1234L);
+        assertEquals(results.get(0).getReactionId(), "reaction id");
+        assertEquals(results.get(0).getMessageHash(), "message hash");
+    }
+
+    @Test
+    public void testGetReactionCounts() {
+        List<ReactionsHandler.Reaction> results = reactionsHandler.getMessageReactions(messageRecord);
+        Map<String, Integer> reactionCounts = reactionsHandler.getReactionCounts(results);
+        assertEquals(reactionCounts.get(":)"), (Integer) 2);
+        assertEquals(reactionCounts.get(":("), (Integer) 1);
     }
 }

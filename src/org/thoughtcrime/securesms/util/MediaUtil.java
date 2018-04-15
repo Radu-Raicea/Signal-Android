f20068fbca72836daa9b7e4721530f319eeee722
veken1199 <super_veko@yahoo.com>
2018-04-14 22:50:00 -0400

    Related to #159: Code cleanup. removed extra indent

--- src/org/thoughtcrime/securesms/util/MediaUtil.java
+++ src/org/thoughtcrime/securesms/util/MediaUtil.java
@@ -41,9 +41,8 @@ public class MediaUtil {
   public static final String AUDIO_UNSPECIFIED = "audio/*";
   public static final String VIDEO_UNSPECIFIED = "video/*";
   public static final String VIDEO_MP4         = "video/mp4" ;
-
-
-    public static @Nullable ThumbnailData generateThumbnail(Context context, MasterSecret masterSecret, String contentType, Uri uri)
+  
+  public static @Nullable ThumbnailData generateThumbnail(Context context, MasterSecret masterSecret, String contentType, Uri uri)
       throws BitmapDecodingException
   {
     long   startMillis = System.currentTimeMillis();
